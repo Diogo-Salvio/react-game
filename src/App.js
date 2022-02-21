f84ea498263d00f52e8f887c1a25f7604650c1d3
Diogo-Salvio <diogosalvio787@gmail.com>
2022-02-21 16:27:59 -0300

    feat: container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 
-import Header from './components/header';
 
+import Header from './components/header';
+import Container from './components/container';
 
 
 
 function App() {
   return (
-      <Header />
+      <div>
+
+        <Header />
+        <Container />
+      
+      </div>
+      
   );
 }
 
---
+++ src/components/container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './container.css'
+
+function Container() {
+    return (
+        <div className="container">
+
+        </div>
+    )
+};
+
+
+export default Container;
