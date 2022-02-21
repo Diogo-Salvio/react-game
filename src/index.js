c3c77a4541c725c40394a40123d2e6fe2cdede7d
Diogo-Salvio <diogosalvio787@gmail.com>
2022-02-21 17:00:55 -0300

    chore: new folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/home';
 
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+import Header from '../../components/header';
+import Container from '../../components/container';
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Header />
+            <Container />
+      
+      </div>
+    );
+}
+
+
+export default Home;
