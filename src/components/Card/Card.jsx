d02e8627def0c87c901924a038d38702d73b6e19
Diogo-Salvio <diogosalvio787@gmail.com>
2022-02-23 13:34:49 -0300

    feat: cards style

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div className="card-style">
+            <h2>Tarefa</h2>
+            <p>Descrição da tarefa</p>
+
+        </div>
+    );
+}
+
+
+export default Card
