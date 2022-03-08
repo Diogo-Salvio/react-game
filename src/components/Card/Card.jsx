f1c99640304c3b224f2c36ad78b7da56cadfc6ef
Diogo-Salvio <diogosalvio787@gmail.com>
2022-03-08 13:32:33 -0300

    feat: exclude button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+
 import './Card.css'
 
 const Card = (props) => {
+    
+    const excludeButton = () => {
+        cardList.slice()
+    }
+    
+    
+    
     return (
         <div className="card-style">
+            <button className="button" onClick={() => excludeButton()}>X</button>
+            
             <input type='text' placeholder="Nome da Tarefa" className="input-tar"/>
+           
             <textarea className="text-area" cols="100%">{props.text}</textarea>
 
         </div>
